fda7c467d44ff4a8fd144c56a3e202d43905eef2
moxunjingmu <1786471868@qq.com>
2022-05-18 22:57:37 +0800

    20.有效的括号

---
+++ leetCode/20.有效的括号.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  let stack = [];
+  let obj = {
+    '(':')',
+    '{':'}',
+    '[':']'
+  } 
+  for (let i = 0; i < s.length; i++) {
+    const ele = s[i];
+    if(ele in obj) {
+      stack.push(ele)
+    } else {
+      // 反括号的场景
+      if(ele!= obj[stack.pop()]) return false
+    }
+  }
+
+  return !stack.length
+
+};
+// @lc code=end
+
