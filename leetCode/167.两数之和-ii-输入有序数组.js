669789e781a39b85e94328055babdf56d37e0807
moxunjingmu <1786471868@qq.com>
2022-06-16 23:49:53 +0800

    leetcode：167.两数之和-Ⅱ-输入有序数组

---
+++ leetCode/167.两数之和-ii-输入有序数组.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  // 递增
+  // 相加之和=target
+  let left = 0;
+  let right = numbers.length-1
+  while(left<=right){
+    let sum = numbers[left]+numbers[right]
+    if(sum === target) {
+      return [left+1, right+1]
+    } else if (sum > target){
+      right--
+    } else {
+      left++
+    }
+  }
+};
+// @lc code=end
+
